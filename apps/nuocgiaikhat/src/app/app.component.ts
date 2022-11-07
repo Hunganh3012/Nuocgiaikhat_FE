c351ac7550d5e81e6e15f0d072dde91e5785b1d8
Hunganh3012 <nguyenhunganh072017@gamil.com>
2022-11-07 10:37:53 +0700

    Initial commit

---
+++ apps/nuocgiaikhat/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nuocgiaikhat-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'nuocgiaikhat';
+}
